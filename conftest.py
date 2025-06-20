44a7042633bac93ac6a5d8cb7432cd78dc7a71c7
Mark Bybin <mbybin@gmail.com>
2025-06-20 17:52:37 +0500

    push again

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 def browser():
     gchr_options = Options()
     gchr_options.add_argument("--window-size=1600,900")
-    #gchr_options.add_argument("--headless")
+    gchr_options.add_argument("--headless")
     driver = webdriver.Chrome(options=gchr_options)
     driver.implicitly_wait(5)
     yield driver
